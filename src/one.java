638db33f8d992d3b3244b8461fb347a0da72f0c8
dumingze <dumingzedeyouxiang@163.com>
2020-04-16 18:06:25 +0800

    Java基础编程练习题
    一共50道题目

---
+++ src/one.java
@@ -0,0 +1,16 @@
+
+public class one {
+
+	public static void main(String[] args) {
+	    int[] arr = new int[20];
+	    arr[0] = 1; // 第一个月兔子的数量，1对
+	    arr[1] = 1; // 第二个月兔子的数量，1对
+	    int sum = 2; // 用于记录兔子总数量，现在初始化为前两个月兔子的总数，2对
+	    for (int i = 2; i < arr.length; i++) {
+	        arr[i] = arr[i - 2] + arr[i - 1];
+	        System.out.println("第" + (i + 1) + "个月兔子对数为：" + arr[i]);
+	        sum += arr[i];
+	    }
+	    System.out.println("兔子总对数：" + sum);
+	}
+}
