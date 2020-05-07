291c634b2c1017b2539fdb80b632c79f3b7a6630
dumingze <dumingzedeyouxiang@163.com>
2020-05-07 10:35:53 +0800

    50th

--- src/Work50.java
+++ src/Work50.java
@@ -1,4 +1,5 @@
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ public class Work50 {
 		// TODO Auto-generated method stub
 Scanner scanner=new Scanner(System.in);
 		int stuid1,stuid2,stuid3;
+		
 		String name1;
 		String name2;
 		String name3;
@@ -25,9 +27,9 @@ Scanner scanner=new Scanner(System.in);
 		System.out.println("请输入第三个学生的学号");
 		stuid3=scanner.nextInt();
 		
-		
 		System.out.println("请出入第一个学生的姓名");
 		name1=scanner.nextLine();
+		
 		System.out.println("请出入第二个学生的姓名");
 		name2=scanner.nextLine();
 		System.out.println("请出入第三个学生的姓名");
@@ -64,9 +66,11 @@ Scanner scanner=new Scanner(System.in);
 		list.add(student1);
 		list.add(student2);
 		list.add(student3);
+		System.out.println(list.get(1).getName());
+		
 		scanner.close();
-		try {
-			FileWriter fw=new FileWriter("/Users/dumingze/desktop/studentgrade.txt");
+/*		try {
+			FileWriter fw=new FileWriter("/Users/dumingze/desktop/stud.txt");
 			for(Student s:list){
 				fw.write((int)s.getAverage());
 				}
@@ -76,7 +80,47 @@ Scanner scanner=new Scanner(System.in);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}*/
+		
+		try {
+			FileWriter fileWriter=new FileWriter("/Users/dumingze/desktop/stud.txt");
+
+	        BufferedWriter bw = new BufferedWriter(fileWriter);
+	        bw.write("No.  "+"Name  "+"grade1  "+"grade2  "+"grade3  "+"average");
+	     
+	        for (int i = 0; i < list.size(); i++) {
+	        	bw.write((int)list.get(i).getAverage());
+				
+			}
+	        bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	
 	}
 }
+/*请输入第一个学生的学号
+1
+请输入第二个学生的学号
+2
+请输入第三个学生的学号
+3
+请出入第一个学生的姓名
+请出入第二个学生的姓名
+dmz
+请出入第三个学生的姓名
+zrw
+请输入第一个学生的成绩
+1
+1
+1
+请输入第二个学生的成绩
+2
+2
+2
+请输入第三个学生的成绩
+3
+3
+3*/
+//程序运行起来有BUG，没有调试成功
